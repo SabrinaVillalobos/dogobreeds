7159d2087b2613db142a9eba46198c12d1e6ffda
sabrinavillalobos <wafflish@gmail.com>
2019-07-21 20:18:27 -0400

    local

--- src/app/services/dogs.service.ts
+++ src/app/services/dogs.service.ts
@@ -7,27 +7,27 @@ import { Observable, of } from 'rxjs';
 })
 export class DogsService {
 
-  constructor(private https: HttpClient) { }
+  constructor(private http: HttpClient) { }
   getRandomPics(): Observable<any> {
 
-    return this.https.get("https://dog.ceo/api/breeds/image/random/5", {});
+    return this.http.get("https://dog.ceo/api/breeds/image/random/5", {});
   }
 
   getAllBreeds(): Observable<any> {
-    return this.https.get("https://dog.ceo/api/breeds/list/all", {});
+    return this.http.get("https://dog.ceo/api/breeds/list/all", {});
   }
 
   getRandomPicsForBreed(breed: string): Observable<any> {
-    return this.https.get(
+    return this.http.get(
       'https://dog.ceo/api/breed/' + breed + '/images/random/5', {});
   }
 
   getSubBreeds(breed: string): Observable<any> {
-    return this.https.get('https://dog.ceo/api/breed/' + breed + '/list/', {});
+    return this.http.get('https://dog.ceo/api/breed/' + breed + '/list/', {});
   }
 
   getRandomPicsForSubBreed(breed: string, subBreed: string): Observable<any> {
-    return this.https.get(
+    return this.http.get(
       'https://dog.ceo/api/breed/' + breed + '/' + subBreed + '/images/random/5', {});
   }
 }
